e60932945829dd69a5b2b5559680ace01246466d
sam-wolfand <sam.wolfand@gmail.com>
2016-01-20 13:35:35 -0800

    wut

--- app/src/main/java/com/samwolfand/unreeld/ui/activity/MovieDetailActivity.java
+++ app/src/main/java/com/samwolfand/unreeld/ui/activity/MovieDetailActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.samwolfand.unreeld.R;
 import com.samwolfand.unreeld.UnreeldApplication;
 import com.samwolfand.unreeld.network.entities.Movie;
@@ -39,11 +42,11 @@ public class MovieDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((UnreeldApplication) getApplication()).getAppComponent().inject(this);
         setContentView(R.layout.activity_movie_detail);
         ButterKnife.bind(this);
-        ((UnreeldApplication) getApplication()).getAppComponent().inject(this);
-
         Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+
 
         ViewCompat.setTransitionName(mContainerInnerItem, TRANSITION_SHARED_ELEMENT);
         if (savedInstanceState == null) {
@@ -52,5 +55,60 @@ public class MovieDetailActivity extends AppCompatActivity {
                     .replace(R.id.movie_details_container, detailFragment, FRAGMENT_TAG)
                     .commit();
         }
+
+        if (getResources().getBoolean(R.bool.two_pane)) {
+            mAppbar.setVisibility(View.GONE);
+        } else {
+            setBackdrop(movie);
+            initCollapsingTitleBehavior(movie.getTitle());
+        }
+
+
     }
+
+    public void setBackdrop(Movie movie) {
+        Glide.with(this).load("http://image.tmdb.org/t/p/w342/" + movie.getBackdropPath())
+                .placeholder(R.color.movie_cover_placeholder)
+                .centerCrop()
+                .crossFade()
+                .into(mBackdrop);
+    }
+
+    private void initCollapsingTitleBehavior(String title) {
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+        }
+        mAppbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            int scrollRange = -1;
+            boolean isShowing = false;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    mCollapsingToolbar.setTitle(title);
+                    isShowing = true;
+                } else if (isShowing) {
+                    mCollapsingToolbar.setTitle("");
+                    isShowing = false;
+                }
+            }
+        });
+    }
+
+    public ImageView getBackdrop() {
+        return mBackdrop;
+    }
+
 }
